f9b9c365446f9f6e12c1a56d1d78f4447cfe5678
dbar2002 <90353137+dbar2002@users.noreply.github.com>
2023-10-14 22:49:15 -0700

    final commit

---
+++ src/MatrixSearch.java
@@ -0,0 +1,42 @@
+public class MatrixSearch {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int row = 0;
+        int col = cols - 1;  // Start from the top-right corner
+
+        while (row < rows && col >= 0) {
+            int current = matrix[row][col];
+            if (current == target) {
+                return true;
+            } else if (current > target) {
+                col--;  // Move left in the current row
+            } else {
+                row++;  // Move down to the next row
+            }
+        }
+
+        return false;  // Target not found in the matrix
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11},
+                {2, 5, 8, 12},
+                {3, 6, 9, 16},
+                {10, 13, 14, 17}
+        };
+
+        int target = 5;
+        boolean found = searchMatrix(matrix, target);
+        if (found) {
+            System.out.println("Element " + target + " found in the matrix.");
+        } else {
+            System.out.println("Element " + target + " not found in the matrix.");
+        }
+    }
+}
